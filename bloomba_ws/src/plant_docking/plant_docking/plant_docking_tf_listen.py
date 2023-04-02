e748edbbd30134acba23454cb265a70714f4ed04
Andrew Zhang <andrewzhang1000@gmail.com>
2023-04-01 23:24:18 -0400

    comments

--- bloomba_ws/src/plant_docking/plant_docking/plant_docking_tf_listen.py
+++ bloomba_ws/src/plant_docking/plant_docking/plant_docking_tf_listen.py
@@ -24,7 +24,7 @@ class FrameListener(Node):
         
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
-        self.publisher = self.create_publisher(Twist, 'turtle2/cmd_vel', 1)
+        self.publisher = self.create_publisher(Twist, 'create3/cmd_vel', 1) #TODO: USE THIS to publish velocities to the Create 3
 
         #Calls on_timer() every 1 second
         self.timer = self.create_timer(0.5, self.tf_on_timer)
@@ -41,6 +41,16 @@ class FrameListener(Node):
             rclpy.time.Time()) #Time at which we want to transform (for now, it's just the latest available transform)
             self.get_logger().info(
             f'\n X pos: {t.transform.translation.x} \n Y pos: {t.transform.translation.y} \n Z pos: {t.transform.translation.z}\n') 
+            
+            correctedX = t.transform.translation.x*4/3 #Correct these values because the sensor is always off by 25% lower
+            correctedY = t.transform.translation.y*4/3
+            correctedZ = t.transform.translation.z*4/3
+
+            self.get_logger().info(
+            f'\n Corrected X pos: {correctedX} \n Corrected Y pos: {correctedY} \n Corrected Z pos: {correctedZ}\n') 
+
+            #TODO: Find the yaw (phi) from a quaternion
+            
 
         except TransformException as ex:
             self.get_logger().info(
